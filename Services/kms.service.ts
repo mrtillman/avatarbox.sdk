68a1726ef254b2057de95eb9cf8c5288d1063ef9
Stephen Tillman <askmrtillman@gmail.com>
2021-01-22 22:34:33 -0500

    refactor + simplify

--- Services/kms.service.ts
+++ Services/kms.service.ts
@@ -1,23 +1,26 @@
 import { KMSClient, EncryptCommand, DecryptCommand } from "@aws-sdk/client-kms";
 
 export class KMSService {
-  private _region: string = "us-east-1";
+  public kmsClient: KMSClient;
+
+  private _region: string;
   private _keyId: string;
 
   constructor(keyId: string) {
     this._keyId = keyId;
+    this._region = "us-east-1";
+    this.kmsClient = new KMSClient({
+      region: this._region,
+    });
   }
 
   public async encrypt(input: string): Promise<string> {
-    const client = new KMSClient({
-      region: this._region,
-    });
     const encryptCommand = new EncryptCommand({
       KeyId: this._keyId,
       Plaintext: Buffer.from(input),
     });
     try {
-      let result = await client.send(encryptCommand);
+      let result = await this.kmsClient.send(encryptCommand);
       return Buffer.from(result.CiphertextBlob as any).toString("base64");
     } catch (error) {
       console.log(error);
@@ -26,15 +29,12 @@ export class KMSService {
   }
 
   public async decrypt(input: string): Promise<string> {
-    const client = new KMSClient({
-      region: this._region,
-    });
     const decryptCommand = new DecryptCommand({
       KeyId: this._keyId,
       CiphertextBlob: Buffer.from(input, "base64"),
     });
     try {
-      let result = await client.send(decryptCommand);
+      let result = await this.kmsClient.send(decryptCommand);
       return Buffer.from(result.Plaintext as any).toString();
     } catch (error) {
       console.log(error);
