1d7870f2950e84542fd1a0ebb6b43019699f1926
Stephen Tillman <askmrtillman@gmail.com>
2021-02-25 19:45:05 -0500

    add bcrypt salt

--- Services/bcrypt.service.ts
+++ Services/bcrypt.service.ts
@@ -2,8 +2,9 @@ import * as bcrypt from "bcrypt";
 
 export class BcryptService {
   async hash(plainText: string): Promise<string> {
+    const salt = process.env.SALT as string;
     return new Promise((resolve, reject) => {
-      bcrypt.hash(plainText, 10, (err, result) => {
+      bcrypt.hash(plainText, salt, (err, result) => {
         if (err) {
           reject(err);
         }
