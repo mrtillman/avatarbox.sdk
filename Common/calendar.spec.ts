5dab9a1338bb0700c1961df8cfa4d158a1bba743
Stephen Tillman <askmrtillman@gmail.com>
2021-03-21 21:26:32 -0400

    expand tests

--- Common/calendar.spec.ts
+++ Common/calendar.spec.ts
@@ -6,10 +6,22 @@ describe("Calendar", () => {
   beforeEach(() => {
     calendar = new Calendar();
   });
-  it("should get yesterday as date", () => {
+  it("should compute yesterday as date", () => {
     const today = moment().startOf("minute").toDate();
     const _today = calendar.today();
     expect(_today).toEqual(today);
+  });
+  it("should compute preceding hour as date", () => {
+    const hours = 1;
+    const now = new Date();
+    const earlierToday = calendar.hoursAgo(hours);
+    expect(earlierToday.getHours()).toBe(now.getHours() - hours);
+  });
+  it("should compute preceding day as date", () => {
+    const days = 1;
+    const now = new Date();
+    const tsEarlierToday = calendar.daysAgo(days);
+    expect(tsEarlierToday.getDate()).toBe(now.getDate() - days);
   });
 });
 
@@ -18,9 +30,21 @@ describe("DynamoDbCalendar", () => {
   beforeEach(() => {
     calendar = new DynamoDbCalendar();
   });
-  it("should get yesterday as timestamp", () => {
+  it("should compute yesterday as timestamp", () => {
     const tsToday = moment().startOf("minute").toDate().getTime().toString();
     const _tsToday = calendar.today();
     expect(_tsToday).toBe(tsToday);
+  });
+  it("should compute preceding hour as timestamp", () => {
+    const hours = 1;
+    const now = new Date();
+    const tsEarlierToday = calendar.hoursAgo(hours);
+    expect((new Date(Number(tsEarlierToday))).getHours()).toBe(now.getHours() - hours);
+  });
+  it("should compute preceding day as timestamp", () => {
+    const days = 1;
+    const now = new Date();
+    const tsEarlierToday = calendar.daysAgo(days);
+    expect((new Date(Number(tsEarlierToday))).getDate()).toBe(now.getDate() - days);
   });
 });
