1864e6eab94ed21771be5959c84a8ed0f81eb2bb
Stephen Tillman <askmrtillman@gmail.com>
2021-03-24 14:23:11 -0400

    finalize calendar tests

--- Common/calendar.spec.ts
+++ Common/calendar.spec.ts
@@ -6,7 +6,20 @@ describe("Calendar", () => {
   beforeEach(() => {
     calendar = new Calendar();
   });
+  it("should compute now as date", () => {
+    const now = new Date();
+    const _now = calendar.now();
+    expect(
+      now.toDateString() == _now.toDateString()
+      && now.getSeconds() == _now.getSeconds()
+    ).toBe(true);
+  })
   it("should compute yesterday as date", () => {
+    const yesterday = moment().subtract(1,"days").toDate().toDateString();
+    const _yesterday = calendar.yesterday().toDateString();
+    expect(_yesterday).toEqual(yesterday);
+  });
+  it("should compute today as date", () => {
     const today = moment().startOf("minute").toDate();
     const _today = calendar.today();
     expect(_today).toEqual(today);
@@ -30,7 +43,21 @@ describe("DynamoDbCalendar", () => {
   beforeEach(() => {
     calendar = new DynamoDbCalendar();
   });
+  it("should compute now as timestamp", () => {
+    const now = new Date();
+    const _now = calendar.now();
+    expect(
+      now.toDateString() == new Date(Number(_now)).toDateString()
+      && now.getSeconds() == new Date(Number(_now)).getSeconds()
+    ).toBe(true);
+  })
   it("should compute yesterday as timestamp", () => {
+    const tsYesterday = moment().subtract(1,"days").toDate().getTime();
+    const _tsYesterday = calendar.yesterday();
+    expect(new Date(Number(_tsYesterday)).toDateString())
+      .toBe(new Date(Number(tsYesterday)).toDateString());
+  });
+  it("should compute today as timestamp", () => {
     const tsToday = moment().startOf("minute").toDate().getTime().toString();
     const _tsToday = calendar.today();
     expect(_tsToday).toBe(tsToday);
