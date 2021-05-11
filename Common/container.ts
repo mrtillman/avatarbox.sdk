cd1370240c4a97d8da7223f7d5a1fd098fd905a0
Stephen Tillman <askmrtillman@gmail.com>
2021-05-10 23:47:44 -0400

    implement twitter client; register twitter dependencies

--- Common/container.ts
+++ Common/container.ts
@@ -8,9 +8,13 @@ const container = awilix.createContainer();
 
 container.register({
   s3: awilix.asClass(S3Service.AvbxIcons),
+  // TODO: improve naming convention / design pattern
+  //       to distinguish between twitter/gravatar
   dynamo: awilix.asClass(DynamoDBService.Gravatar),
+  dynamoTwitter: awilix.asClass(DynamoDBService.Twitter),
   sqs: awilix.asClass(SQSService),
   user: awilix.asClass(UserService.Gravatar),
+  userTwitter: awilix.asClass(UserService.Twitter),
 });
 
 export { container };
--- Presentation/Clients/twitter.ts
+++ Presentation/Clients/twitter.ts
@@ -1,10 +1,22 @@
+import { container } from '../../Common/container';
+import { TwitterProfile } from '../../Domain/twitter-profile';
+import { UserService } from '../../Services/user.service';
+
 export class AvbxTwitterClient {
+  private _user: UserService.Twitter;
   private _token:string;
   private _tokenSecret:string;
 
   constructor(token:string, tokenSecret:string){
     this._token = token;
     this._tokenSecret = tokenSecret;
+    this._user = container.resolve("userTwitter");
+  }
+
+  async sync(twitterProfile: TwitterProfile): Promise<void> {
+    // TODO: if Twitter user exists update username, token, tokenSecret
+    //       otherwise, save
+    await this._user.save(twitterProfile);
   }
 
   on(){
@@ -26,4 +38,10 @@ export class AvbxTwitterClient {
   removeImage(id:string){
 
   }
+
+  touch(id:string){
+    
+  }
+
+  // delete, collect, peek, dig, sweep reset
 }
