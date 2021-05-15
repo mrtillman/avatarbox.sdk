ba292a80e2dfae37700dbee2e4858a8a9e4184a8
Stephen Tillman <askmrtillman@gmail.com>
2021-05-15 14:10:24 -0400

    stub unit tests for twitter client

---
+++ Presentation/Clients/twitter.spec.ts
@@ -0,0 +1,79 @@
+import { container } from "../../Common/container";
+import { AvbxTwitterClient } from "./twitter";
+import * as awilix from "awilix";
+
+container.register({
+  kms: awilix.asValue({
+    encrypt: jest.fn(),
+    decrypt: jest.fn(),
+  }),
+  s3: awilix.asValue({
+    putIcon: jest.fn(),
+    deleteIcons: jest.fn(),
+  }),
+  twitterRepo: awilix.asValue({
+    collect: jest.fn(),
+    peek: jest.fn(),
+    dig: jest.fn(),
+    reset: jest.fn(),
+    sweep: jest.fn(),
+  }),
+  sqs: awilix.asValue({
+    touch: jest.fn(),
+  }),
+  twitterUserService: awilix.asValue({
+    save: jest.fn(),
+    find: jest.fn(),
+    findById: jest.fn(),
+    getClient: jest.fn(),
+    on: jest.fn(),
+    off: jest.fn(),
+    delete: jest.fn(),
+  }),
+});
+const userId = "asdf";
+const imageUrl = "https://via.placeholder.com/300";
+const mockTwitterClient = () => ({} as any);
+
+describe("AvbxTwitterClient", () => {
+  let avbxClient: AvbxTwitterClient;
+  beforeEach(() => {
+    avbxClient = new AvbxTwitterClient();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should collect", async () => {
+    const collect = avbxClient.repo.collect as jest.Mock;
+    collect.mockReturnValue(1);
+
+    const result = await avbxClient.collect();
+
+    expect(result).toBeDefined();
+  });
+  it("should peek", async () => {
+    const peek = avbxClient.repo.peek as jest.Mock;
+    peek.mockReturnValue(1);
+
+    const result = await avbxClient.collect();
+
+    expect(result).toBeDefined();
+  });
+  it("should sweep", async () => {
+    const userIds = [1, 2];
+    const deleteIcons = avbxClient.s3.deleteIcons as jest.Mock;
+    const sweep = avbxClient.repo.sweep as jest.Mock;
+    sweep.mockReturnValue(userIds);
+
+    await avbxClient.sweep();
+
+    expect(deleteIcons.mock.calls[0]).toEqual(userIds);
+  });
+  it("should touch", async () => {
+    const touch = avbxClient.sqs.touch as jest.Mock;
+
+    await avbxClient.touch(userId);
+
+    expect(touch.mock.calls[0]).toEqual([userId]);
+  });
+});
